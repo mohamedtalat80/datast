6309300b9fd25dc83768a9abe4537154ca11ad18
mohamedtalat80 <80793810+mohamedtalat80@users.noreply.github.com>
2021-03-18 03:16:05 +0200

    Add files via upload

---
+++ mohamed taalat.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int const cap =10;
+class list(){
+int size ;
+int arr[cap]
+public:
+list(int size){
+size=0;
+}
+int get(int index){//the get method
+ int index=size -1;
+if (index>=size,index<0,index=cap
+    cout<<"the number is not found"
+       return -1;
+        )
+ return arr[index];
+}
+ bool set(int index ,int value,int pos ){
+int index =index -1;
+if (index>=size,index<0,index=cap
+    cout<<"The number is not exist"
+       return -1;  )
+for(index<size,index=pos,pos--
+
+
+}
+}
+int main(){
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
